2711008e3ed8a31b7244d662a37346372f0efd5b
Aldhair Vera <aldhairvera15@gmail.com>
2023-09-12 22:09:26 -0500

    Adding i18n for react and set the language files - Also using localStorage to save data preference in language

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,17 @@
 import logOut from "../functions/logOut";
+import { useTranslation } from "react-i18next";
 
 export default function Home() {
+  const { t } = useTranslation(["translation"]);
+
   return (
     <div>
       <h1 className="text-3xl font-bold mb-3">Hola</h1>
       <a
         onClick={logOut}
-        className="w-full mt-4 bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded">
-        Cerrar sesion
+        className="w-full mt-4 bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded"
+      >
+        {t("logout")}
       </a>
     </div>
   );
