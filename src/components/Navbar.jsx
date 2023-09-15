5419324beadacc723bcb77bd38ba57ec329f99ab
Justin <justinvelasquezaguirre@gmail.com>
2023-09-14 21:11:38 -0500

    Error in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,8 +3,11 @@ import { auth } from "../firebase/connections";
 import { onAuthStateChanged } from "firebase/auth";
 import { Link } from "react-router-dom";
 import logOut from "../functions/logOut";
+import { useTranslation } from "react-i18next";
 
 export function Navbar() {
+  const { t } = useTranslation(["translation"]);
+
   const [user, setUser] = useState(null);
 
   onAuthStateChanged(auth, (currentUser) => {
@@ -17,16 +20,16 @@ export function Navbar() {
 
   return user ? (
     <div>
-      <Link to="profile">Profile</Link>
+      <Link to="profile">{t("profile")}</Link>
       <a
         onClick={logOut}
         className="w-full mt-4 bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded">
-        Cerrar sesion
+        {t("logout")}
       </a>
     </div>
   ) : (
     <div>
-      <Link to="login">Login</Link>
+      <Link to="login">{t("login")}</Link>
     </div>
   );
 }
