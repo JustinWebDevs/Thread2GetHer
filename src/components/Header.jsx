8e41754593d24eb7c4528e37e798708996bf4ec7
Aldhair Vera <aldhairvera15@gmail.com>
2023-09-15 11:38:01 -0500

    Solve loading problem

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,9 +9,11 @@ export function Header() {
       <div className="header p-2 flex flex-row justify-between items-center sticky top-0 bg-slate-500 ">
         <div
           className="profile rounded-full bg-red-400 w-12 h-12"
+          style={{ cursor: "pointer" }}
           onClick={() => {
             setShowNavbar(!showNavbar);
-          }}></div>
+          }}
+        ></div>
         <h1 className=" text-lg lg:text-3xl">Thread2GetHer</h1>
         <h1 className="text-xl">Buscar</h1>
       </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { auth } from "../firebase/connections";
 import { onAuthStateChanged } from "firebase/auth";
 import { Link } from "react-router-dom";
@@ -10,20 +10,23 @@ export function Navbar() {
 
   const [user, setUser] = useState(null);
 
-  onAuthStateChanged(auth, (currentUser) => {
-    if (currentUser) {
-      setUser(currentUser);
-    } else {
-      setUser(null);
-    }
-  });
+  useEffect(() => {
+    onAuthStateChanged(auth, (currentUser) => {
+      if (currentUser) {
+        setUser(currentUser);
+      } else {
+        setUser(null);
+      }
+    });
+  }, []);
 
   return user ? (
     <div>
       <Link to="profile">{t("profile")}</Link>
       <a
         onClick={logOut}
-        className="w-full mt-4 bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded">
+        className="w-full mt-4 bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded"
+      >
         {t("logout")}
       </a>
     </div>
