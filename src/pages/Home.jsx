fa7b2155ec9bb06dda8c87ccd4b021bec5ac7341
Aldhair Vera <aldhairvera15@gmail.com>
2023-09-28 16:46:28 -0500

    Fix home stuff

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,17 +6,43 @@ import Thread from "../components/Thread";
 
 let tempThreadData = [
   {
-    id: 1024368957,
+    id: "ff1a2d6a-bfcc-4c05-aa16-e73e6452b128",
     title: "[求助] 如何在vue中使用echarts",
     content: "<p>我想在vue中使用echarts，但是不知道怎么引入。有没有人能帮忙看",
+    timeStamp: 1695937387882,
     image: "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg",
+    reactions: {
+      thread: 4,
+      share: {
+        value: 1,
+        isShared: false,
+      },
+      like: {
+        value: 2,
+        isLiked: false,
+      },
+      view: 4,
+    },
   },
   {
-    id: 1024368954,
+    id: "ff1a2d8a-bfcc-4c05-aa16-e73e6452b538",
     title: "Sarah Logan 😀",
     content: "<p>我想在vue中使用echarts，但是不知道怎么引入。有没有人能帮忙看",
+    timeStamp: 1695937387871,
     image:
       "https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    reactions: {
+      thread: 4,
+      share: {
+        value: 1,
+        isShared: false,
+      },
+      like: {
+        value: 2,
+        isLiked: false,
+      },
+      view: 4,
+    },
   },
 ];
 
@@ -39,9 +65,14 @@ export default function Home() {
         {tempThreadData.map((item, index) => (
           <Thread
             key={`${index}`}
+            id={item.id}
             title={item.title}
             content={item.content}
             image={item.image}
+            timeStamp={item.timeStamp}
+            deleteThread={null}
+            updateThreads={null}
+            reactions={item.reactions}
           />
         ))}
       </div>
