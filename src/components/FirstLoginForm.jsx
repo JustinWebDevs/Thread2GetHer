d63effcf35ea53e4710efee082ccf22368f3c3ff
Justin <127259451+JustinWebDevs@users.noreply.github.com>
2023-10-05 17:57:19 -0500

    firstLogin Interests

--- src/components/FirstLoginForm.jsx
+++ src/components/FirstLoginForm.jsx
@@ -1,9 +1,9 @@
 import { doc, setDoc } from "firebase/firestore";
 import { db } from "../firebase/connections";
 import { useTranslation } from "react-i18next";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { FaArrowRightLong, FaArrowLeftLong } from "react-icons/fa6";
-import { InterestsCard } from "./interestsCard";
+// import { InterestsCard } from "./interestsCard";
 
 export function FirstLoginForm(userId) {
   const { t } = useTranslation(["translation"]);
@@ -13,6 +13,32 @@ export function FirstLoginForm(userId) {
   const [lastName, setLastName] = useState("");
   const [gender, setGender] = useState(0);
   const [born, setBorn] = useState(null);
+  const [interest, setInterest] = useState([]);
+  const [age, setAge] = useState(0);
+
+  useEffect(() => {
+  if (born){
+  // Divide la fecha en partes (año, mes, día)
+  const partesFecha = born.split("-");
+  
+  // Obtiene el año, mes y día de la fecha de nacimiento
+  const anioNacimiento = parseInt(partesFecha[0]);
+  const mesNacimiento = parseInt(partesFecha[1]) - 1; // Los meses en JavaScript van de 0 a 11
+  const diaNacimiento = parseInt(partesFecha[2]);
+  
+  // Obtiene la fecha actual
+  const fechaActual = new Date();
+  
+  // Calcula la fecha de nacimiento
+  const fechaNac = new Date(anioNacimiento, mesNacimiento, diaNacimiento);
+  
+  // Calcula la diferencia en milisegundos entre la fecha actual y la fecha de nacimiento
+  const diferenciaMilisegundos = fechaActual - fechaNac;
+  
+  // Calcula la edad en años
+  setAge(Math.floor(diferenciaMilisegundos / (1000 * 60 * 60 * 24 * 365.25)));
+  }
+  }, [born]);
 
   const addUser = async (e) => {
     e.preventDefault();
@@ -21,11 +47,10 @@ export function FirstLoginForm(userId) {
       await setDoc(doc(db, `users/${userId}`), {
         first: name,
         last: lastName,
-        born: 1815,
-        age: 20,
-        gender: 1,
-        interest: [2, 3],
-        userName: "Enated",
+        born: born,
+        age: age,
+        gender: gender,
+        interest: interest,
       });
 
       history(`/home`);
@@ -63,6 +88,16 @@ export function FirstLoginForm(userId) {
   const thirdStep = () => {
     if (isValidData(name) && isValidData(lastName)) setProgress(progress + 1);
   };
+
+  const addToArray = (x) => {
+    if (interest.includes(x)){
+      const newInterest = interest.filter((elemento) => elemento !== x);
+
+      setInterest(newInterest);
+    }else{
+      setInterest([...interest, x]);
+    }
+  }
 
   return (
     <div className=" bg-gradient-to-r from-blue-500 to-blue-600 h-screen w-full flex flex-col items-center text-white font-bold text-3xl lg:text-5xl text-center mb-4 lg:mb-8">
@@ -183,7 +218,19 @@ export function FirstLoginForm(userId) {
             progress == 2 ? "" : "hidden"
           } flex flex-col items-center justify-center h-full `}>
           <div className="flex align-baseline m-2">
-            <InterestsCard icon="" tittle="" description="" />
+            {/* <InterestsCard /> */}
+            <div className="flex">
+              <input type="checkbox" name="interest" value="1" onClick={() => {addToArray(1)}} />
+              <label className="ml-2">Cine</label>
+            </div>
+            <div className="flex">
+              <input type="checkbox" name="interest" value="2" onClick={() => {addToArray(2)}} />
+              <label className="ml-2">Musica</label>
+            </div>
+            <div className="flex">
+              <input type="checkbox" name="interest" value="3" onClick={() => {addToArray(3)}} />
+              <label className="ml-2">Mascotas</label>
+            </div>
           </div>
           <div className="flex align-baseline m-2"></div>
           <div className="flex justify-around w-2/4">
@@ -193,9 +240,10 @@ export function FirstLoginForm(userId) {
               <FaArrowLeftLong />
             </button>
             <button
+             type="submit"
               className=" bg-blue-400 hover:bg-blue-500 py-2 px-4 text-white rounded my-4"
               onClick={thirdStep}>
-              <FaArrowRightLong />
+              Enviar
             </button>
           </div>
         </div>
--- src/components/InterestsCard.jsx
+++ src/components/InterestsCard.jsx
@@ -1,11 +1,16 @@
-export function InterestsCard(icon, tittle, description) {
+export function InterestsCard() {
   return (
-    <a
-      href="#"
-      className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-      <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-        Noteworthy technology acquisitions 2021
-      </h5>
-    </a>
+    <div className="flex flex-wrap">
+      <a
+        href="#"
+        className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+          Noteworthy technology acquisitions 2021
+        </h5>
+      </a>
+      <div className="bg-gray-800 border-gray-700 hover:bg-gray-700 w-24 rounded-lg">
+        a
+      </div>
+    </div>
   );
 }
