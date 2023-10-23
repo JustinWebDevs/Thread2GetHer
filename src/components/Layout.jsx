8e0663f420c400c1b5d26b8cbbd42e3cc5817e9f
Aldhair Vera <aldhairvera15@gmail.com>
2023-10-22 22:43:09 -0500

    Redirection

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,8 +1,27 @@
 import { Outlet } from "react-router-dom";
 import { Header } from "./Header";
 import { Footer } from "./Footer";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+// import { onAuthStateChanged } from "firebase/auth";
+// import { auth } from "../firebase/connections";
 
 export const Layout = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    // if (!isLoggedIn()) {
+    //   navigate("/login", { replace: true });
+    // }
+    const user = localStorage.getItem("user");
+
+    if (user) {
+      navigate("/login", { replace: true });
+    } else {
+      navigate("/home", { replace: true });
+    }
+  }, []);
+
   return (
     <>
       <Header />
